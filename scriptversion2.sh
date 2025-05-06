93e1aa7cb8fa2a77e82bab5b915aaf409dd8e53a
VijayKollati234 <kollativijaykumar.grow@gmail.com>
2025-05-06 08:29:16 +0530

    scriptversion.sh

---
+++ scriptversion2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+########################
+# Author : vijay
+# Date : 5/6/2025
+#########################
+
+myname="Vijay"
+myage="33"
+
+echo "Hello, my name is $myname."
+echo "I'm $myage years old."
