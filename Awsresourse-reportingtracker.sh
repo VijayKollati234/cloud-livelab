eb360179b4eec6b39c148b92484d3b1ff7e06ea0
VijayKollati234 <kollativijaykumar.grow@gmail.com>
2025-05-10 18:29:30 +0530

    AWS reporting tracker script

---
+++ Awsresourse-reportingtracker.sh
@@ -0,0 +1,16 @@
+#############################
+# Author : Vijay
+# Date : 05/10/2025
+#
+# version : V4
+#
+# This script will report the AWS resource usage
+##############################
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+# List s3 buckets
+aws S3 ls
