d041a6da91f09599c21a410f0e04daa1408a34a0
VijayKollati234 <kollativijaykumar.grow@gmail.com>
2025-05-10 19:43:13 +0530

    updated-version2 Awsresourse-reportingtracker.sh

--- Awsresourse-reportingtracker.sh
+++ Awsresourse-reportingtracker.sh
@@ -6,6 +6,7 @@
 #
 # This script will report the AWS resource usage
 ##############################
+set -x
 
 # AWS S3
 # AWS EC2
@@ -13,13 +14,17 @@
 # AWS IAM Users
 
 # List s3 buckets
-aws S3 ls
+echo "Print list of s3 buckets"
+aws S3 ls >> resourse
 
 # List EC2 Instance
-aws ec2 describe-instances
+echo "Print list of ec2 describe-instances"
+aws ec2 describe-instances >> resourse
 
 # List lambda
-aws lambda list-functions
+echo "Print list of lambda list-functions"
+aws lambda list-functions >> resourse
 
 # List IAM users
-aws iam list-users
+echo "Print list of IAM users"
+aws iam list-users >> resourse
