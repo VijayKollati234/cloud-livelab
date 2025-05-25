ba40766cf285bf3f9c2521f0887984eddb150756
VijayKollati234 <kollativijaykumar.grow@gmail.com>
2025-05-25 14:36:49 +0530

    update-ansible

--- ansible.sh
+++ ansible.sh
@@ -3,4 +3,26 @@ second method for passwordless authentication :- connect to instance and give th
 :-sudo vim /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 sudo systemctl restrat ssh
 sudo systemctl deamon-reload
-anisble -i inventory.ini -m ping all
+ansible -i inventory.ini -m ping all
+ansible -i inventory.ini -m shell -a 
+adhoc : ansible inventory.ini -i shell -a "sudo " all
+
+---
+- name: Install Apache HTTPD and deploy config
+  hosts: all
+  become: yes
+
+  tasks:
+    - name: Install apache httpd
+      apt:
+        name: apache2
+        state: present
+        update_cache: yes
+
+    - name: Copy file with owner and permissions
+      copy:
+        src: files/my.conf
+        dest: /etc/myapp/my.conf
+        owner: root
+        group: root
+        mode: '0644'
